aac72cb7734fe0d011b8323f8d0b41a9ea6ad6d1
l-borkowski <lukasz.borkowski2525@gmail.com>
2022-03-23 11:10:42 +0100

    feat: add command_runner

---
+++ lib/salami_cli.dart
@@ -0,0 +1,10 @@
+/// A Salami Command Line Interface for Dart
+///
+/// ```sh
+/// # activate salami_cli
+/// pub global activate salami_cli
+///
+/// # see usage
+/// salami --help
+/// ```
+library salami_cli;
---
+++ lib/src/command_runner.dart
@@ -0,0 +1,53 @@
+import 'package:args/args.dart';
+import 'package:args/command_runner.dart';
+import 'package:io/io.dart';
+import 'package:mason/mason.dart';
+
+import 'package:salami_cli/src/version.dart';
+
+/// {@template salami_command_runner}
+/// A [CommandRunner] for the Very Good CLI.
+/// {@endtemplate}
+class SalamiCommandRunner extends CommandRunner<int> {
+  /// {@macro salami_command_runner}
+  SalamiCommandRunner({Logger logger})
+      : _logger = logger ?? Logger(),
+        super('salami', '🚀 A Salami Commandline Interface') {
+    argParser.addFlag(
+      'version',
+      negatable: false,
+      help: 'Print the current version.',
+    );
+  }
+
+  final Logger _logger;
+
+  @override
+  Future<int> run(Iterable<String> args) async {
+    try {
+      final _argResults = parse(args);
+      return await runCommand(_argResults) ?? ExitCode.success.code;
+    } on FormatException catch (e) {
+      _logger
+        ..err(e.message)
+        ..info('')
+        ..info(usage);
+      return ExitCode.usage.code;
+    } on UsageException catch (e) {
+      _logger
+        ..err(e.message)
+        ..info('')
+        ..info(usage);
+      return ExitCode.usage.code;
+    }
+  }
+
+  @override
+  Future<int> runCommand(ArgResults topLevelResults) async {
+    if (topLevelResults['version'] == true) {
+      _logger.info('salami version: $packageVersion');
+      return ExitCode.success.code;
+    }
+    return super.runCommand(topLevelResults);
+  }
+}
